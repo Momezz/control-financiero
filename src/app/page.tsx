f494a6610ed53f3c5558ff123f19f6ab7fae9912
Momezz <julgomez14gmail.com>
2024-04-08 08:22:21 -0500

    I put the boards in a container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
 import Banner from '@/components/Banner/Banner';
 import TransactionForm from '@/components/TransactionForm/TransactionForm';
-import ResultsTable from '@/components/ResultsTable/ResultsTable';
+import TableContainer from '@/components/TableContainer/TableContainer';
 
 export default function Home() {
-  const datosEconomicos = [
-    { item: 'Producto A', value: 400, percentage: 5 },
-    { item: 'Producto B', value: 200, percentage: 15 },
-    { item: 'Producto C', value: 150, percentage: 12 },
-  ];
 
   return (
     <main>
       <div className='w-full h-200px'>
         <Banner />
         <TransactionForm />
-        <ResultsTable data={datosEconomicos} />
+        <TableContainer />
       </div>
     </main>
   );
