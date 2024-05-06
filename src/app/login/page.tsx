d8fa6f8a1746614e9a7027154902ecaeee158ebd
Momezz <julgomez14@gmail.com>
2024-05-05 21:37:02 -0500

    I create graphics to add effects

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import LoginForm from '@/components/LoginForm/LoginForm';
+import EffectBars from '@/components/EffectBars/EffectBars';
 import styles from './login.module.css';
 
 const Login = () => {
@@ -6,6 +7,7 @@ const Login = () => {
     <main>
       <article className={styles.login__container}>
         <LoginForm />
+        <EffectBars />
       </article>
     </main>
   )
